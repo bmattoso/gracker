dffab0cf7de4916273603073bc04342615ca350a
Bruno Mattoso Gonçalves <bruno.mattoso.goncalves@gmail.com>
2023-07-04 14:44:36 -0300

    Add Kover to generate code coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.kover) apply false
 }
 
 true // Needed to make the Suppress annotation work for the plugins block
