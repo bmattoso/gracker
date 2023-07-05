147b23fd405274cce457332251ed0428c6a97576
Bruno Mattoso Gonçalves <bruno.mattoso.goncalves@gmail.com>
2023-07-04 21:41:01 -0300

    Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.detekt) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kover) apply false
 }
