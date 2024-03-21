b9ab50c4f7e1c7b969038d18fa720e35f5d19e81
Bruno Mattoso Goncalves <bruno.goncalves@swile.com.br>
2024-03-21 19:57:00 -0300

    Update libraries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,8 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,6 +15,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Gracker"
+rootProject.name = "gracker"
 include(":app")
  
