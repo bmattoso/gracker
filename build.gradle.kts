8156557903df9621624af72b89b900c623f65a80
Bruno Mattoso Goncalves <bruno.mattoso.goncalves@gmail.com>
2024-03-25 16:41:45 -0300

    Configure initial setup detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,20 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.detekt)
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kover) apply false
 }
+
+subprojects {
+    apply {
+        plugin(rootProject.libs.plugins.detekt.get().pluginId)
+    }
+
+    detekt {
+        config.setFrom(rootProject.file("config/detekt/detekt.yml"))
+        buildUponDefaultConfig = true
+    }
+}
+
 
 true // Needed to make the Suppress annotation work for the plugins block
