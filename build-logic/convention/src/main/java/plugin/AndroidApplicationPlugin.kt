9de09b1aa513cd4d12fe06ee04657fb8adbe6017
Bruno Mattoso Goncalves <bruno.mattoso.goncalves@gmail.com>
2024-03-27 11:24:36 -0300

    Create build logic module

---
+++ build-logic/convention/src/main/java/plugin/AndroidApplicationPlugin.kt
@@ -0,0 +1,19 @@
+package plugin
+
+import extension.configureAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AndroidApplicationPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            configureAndroid()
+        }
+    }
+}
