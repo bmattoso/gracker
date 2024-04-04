08ea03446361ccf4054a048c86dd7bef375f008a
Bruno Mattoso Goncalves <bruno.mattoso.goncalves@gmail.com>
2024-04-04 16:05:29 -0300

    Create snapshot module

---
+++ snapshot/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("gracker.android.library")
+    alias(libs.plugins.paparazzi)
+}
+
+android {
+    namespace = "br.com.gracker.snapshot"
+}
+
+dependencies {
+//    implementation(project(":history"))
+}
+
+plugins.withId("app.cash.paparazzi") {
+    afterEvaluate {
+        dependencies.constraints {
+            add("testImplementation", "com.google.guava:guava") {
+                attributes {
+                    attribute(
+                        TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
+                        objects.named(TargetJvmEnvironment::class.java, TargetJvmEnvironment.STANDARD_JVM),
+                    )
+                }
+                because(
+                    "LayoutLib and sdk-common depend on Guava's -jre published variant." +
+                        "See https://github.com/cashapp/paparazzi/issues/906.",
+                )
+            }
+        }
+    }
+}
