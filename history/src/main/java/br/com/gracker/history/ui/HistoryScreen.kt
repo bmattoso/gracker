687261d24587b3fa666007fa48bb5ded476bb216
Bruno Mattoso Gonçalves <bruno.mattoso.goncalves@gmail.com>
2024-04-07 18:55:43 -0300

    Add initial snapshot from history screen

--- history/src/main/java/br/com/gracker/history/ui/HistoryScreen.kt
+++ history/src/main/java/br/com/gracker/history/ui/HistoryScreen.kt
@@ -1,13 +1,23 @@
 package br.com.gracker.history.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HistoryScreenContent() {
-    Text(
-        text = "No game history",
-        color = Color.Black,
-    )
+    Column(
+        modifier = Modifier.background(Color.White),
+    ) {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "No game history",
+            color = Color.Black,
+        )
+    }
 }
