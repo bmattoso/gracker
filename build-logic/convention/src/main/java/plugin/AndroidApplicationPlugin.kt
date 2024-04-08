4e7ba20a71349518cfdffce9b7f2f9715e322780
Bruno Mattoso Goncalves <bruno.mattoso.goncalves@gmail.com>
2024-04-08 18:37:16 -0300

    Replace string plugin id by helper function

--- build-logic/convention/src/main/java/plugin/AndroidApplicationPlugin.kt
+++ build-logic/convention/src/main/java/plugin/AndroidApplicationPlugin.kt
@@ -3,6 +3,7 @@ package plugin
 import com.android.build.api.dsl.CommonExtension
 import extension.configureAndroid
 import extension.configureCompose
+import extension.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
@@ -13,7 +14,7 @@ class AndroidApplicationPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.android.application")
-                apply("org.jetbrains.kotlin.android")
+                apply(plugin("kotlinAndroid").pluginId)
             }
 
             configureAndroid()
--- build-logic/convention/src/main/java/plugin/AndroidLibraryPlugin.kt
+++ build-logic/convention/src/main/java/plugin/AndroidLibraryPlugin.kt
@@ -3,6 +3,7 @@ package plugin
 import com.android.build.api.dsl.CommonExtension
 import extension.configureAndroid
 import extension.configureCompose
+import extension.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
@@ -13,8 +14,8 @@ class AndroidLibraryPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.android.library")
-                apply("org.jetbrains.kotlin.android")
-                apply("org.jetbrains.kotlinx.kover")
+                apply(plugin("kotlinAndroid").pluginId)
+                apply(plugin("kover").pluginId)
             }
 
             configureAndroid()
